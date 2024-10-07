6b3c7f42c96462966a6f864ed781cd8c3323d93c
AlexMqs <alexandremarques73000@gmail.com>
2024-10-07 18:22:43 +0200

    Ajout du background avec tsparticles

---
+++ src/Pages/Home/home.js
@@ -0,0 +1,13 @@
+import './home.scss';
+
+function Home() {
+  return (
+    <section id="home">
+      <div>
+        <h1>Accueil</h1>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss'; 
+import Home from './Pages/Home/home.js'; 
+import ParticlesComponent from './Components/Particles/particles.jsx';
+import Navbar from './Components/NavBar/navbar.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ParticlesComponent id="particles" />
+    <Navbar />
+    <Home />  
   </React.StrictMode>
 );
