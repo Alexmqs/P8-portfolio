d6c939ca787bd5b0c32a795f3d1c23f971ba5e94
AlexMqs <alexandremarques73000@gmail.com>
2024-10-08 17:37:19 +0200

    Mise en place des base de About

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.scss'; 
 import Home from './Pages/Home/home.js'; 
 import ParticlesComponent from './Components/Particles/particles.jsx';
-import Navbar from './Components/NavBar/navbar.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ParticlesComponent id="particles" />
-    <Navbar />
     <Home />  
   </React.StrictMode>
 );
