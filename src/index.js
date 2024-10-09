500d45f2d963bd0287592f1015a344252fae8a95
AlexMqs <alexandremarques73000@gmail.com>
2024-10-09 21:09:31 +0200

    Routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importer le routing
 import './index.scss'; 
-import Home from './Pages/Home/home.js'; 
-import ParticlesComponent from './Components/Particles/particles.jsx';
-
+import Home from './Pages/Home/home'; // Page d'accueil
+import ProjectDetails from './Pages/Project/project'; // Utilise le chemin correct vers project.jsx
+import ParticlesComponent from './Components/Particles/particles.jsx'; // Particules pour l'arrière-plan
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ParticlesComponent id="particles" />
-    <Home />  
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />  {/* Page d'accueil */}
+        <Route path="/projets/:id" element={<ProjectDetails />} /> {/* Page pour chaque projet */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
